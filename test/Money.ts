4e47bf39fa1acb8c36936901083bb23bb9c40191
Youn Se Eun <tpdms717@gmail.com>
2023-03-11 08:31:19 +0900

    Money Class

---
+++ test/Money.ts
@@ -0,0 +1,37 @@
+class Money {
+  private BigDecimal: number
+  static ZERO = Money.wons(0)
+
+  constructor(BigDecimal: number, ZERO: number) {
+    this.BigDecimal = BigDecimal
+    this.ZERO = ZERO
+  }
+
+  static wons(amount: number) {
+    return new Money(BigDecimal.valueOf(amount))
+  }
+
+  Money(amount: number) {
+    this.amount = amount
+  }
+
+  plus(amount: number) {
+    return new Money(this.amount.add(amount.amount))
+  }
+
+  minus(amount: number) {
+    return new Money(this.amount.subtract(amount.amount))
+  }
+
+  times(percent: number) {
+    return new Money(this.amount.multiply(BigDecimal.valueOf(percent)))
+  }
+
+  isLessThan(other: number) {
+    return amount.compareTo(other.amount) < 0
+  }
+
+  isGreaterThanOrEqual(other: number) {
+    return amount.compareTo(other.amount) >= 0
+  }
+}
