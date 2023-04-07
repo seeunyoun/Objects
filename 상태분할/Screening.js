5a0c8a59be93603513269f21b1dc2c2e30a717b9
Youn Se Eun <tpdms717@gmail.com>
2023-04-08 08:40:55 +0900

    Create Screening class

---
+++ 상태분할/Screening.js
@@ -0,0 +1,26 @@
+'use strict'
+class Screening2 {
+  constructor(movie, sequence, whenScreened) {
+    this.movie = movie
+    this.sequence = sequence
+    this.whenScreened = whenScreened
+  }
+  getMovie() {
+    return this.movie
+  }
+  setMovie(movie) {
+    this.movie = movie
+  }
+  getWhenScreened() {
+    return this.whenScreened
+  }
+  setWhenScreened(whenScreened) {
+    this.whenScreened = whenScreened
+  }
+  getSequence() {
+    return this.sequence
+  }
+  setSequence(sequence) {
+    this.sequence = sequence
+  }
+}
---
+++ 상태분할/Screening.ts
@@ -0,0 +1,35 @@
+class Screening2 {
+  private movie: string
+  private sequence: number
+  private whenScreened: number
+
+  constructor(movie: string, sequence: number, whenScreened: number) {
+    this.movie = movie
+    this.sequence = sequence
+    this.whenScreened = whenScreened
+  }
+
+  getMovie() {
+    return this.movie
+  }
+
+  setMovie(movie: string) {
+    this.movie = movie
+  }
+
+  getWhenScreened() {
+    return this.whenScreened
+  }
+
+  setWhenScreened(whenScreened: number) {
+    this.whenScreened = whenScreened
+  }
+
+  getSequence() {
+    return this.sequence
+  }
+
+  setSequence(sequence: number) {
+    this.sequence = sequence
+  }
+}
