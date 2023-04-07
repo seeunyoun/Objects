9c045ec5ccd5937d8e3315f34846850d08e62d91
Youn Se Eun <tpdms717@gmail.com>
2023-04-08 08:26:53 +0900

    Create Movie class

---
+++ 상태분할/Movie.js
@@ -0,0 +1,50 @@
+'use strict'
+class Movie2 {
+  constructor(
+    title,
+    runningTime,
+    fee,
+    discountConditions,
+    movieType,
+    discountAmount,
+    discountPercent,
+  ) {
+    this.title = title
+    this.runningTime = runningTime
+    this.fee = fee
+    this.discountConditions = discountConditions
+    this.movieType = movieType
+    this.discountAmount = discountAmount
+    this.discountPercent = discountPercent
+  }
+  getMovieType() {
+    return this.movieType
+  }
+  setMovieType(movieType) {
+    this.movieType = movieType
+  }
+  getFee() {
+    return this.fee
+  }
+  setFee(fee) {
+    this.fee = fee
+  }
+  getDiscountConditions() {
+    return Collections.unmodifiableList(this.discountConditions)
+  }
+  setDiscountConditions(discountConditions) {
+    this.discountConditions = discountConditions
+  }
+  getDiscountAmount() {
+    return this.discountAmount
+  }
+  setDiscountAmount(discountAmount) {
+    this.discountAmount = discountAmount
+  }
+  getDiscountPercent() {
+    return this.discountPercent
+  }
+  setDiscountPercent(discountPercent) {
+    this.discountPercent = discountPercent
+  }
+}
---
+++ 상태분할/Movie.ts
@@ -0,0 +1,79 @@
+interface movieType {
+  AMOUNT_DISCOUNT: 'AMOUNT_DISCOUNT'
+  PERCENT_DISCOUNT: 'PERCENT_DISCOUNT'
+  NONE_DISCOUNT: 'NONE_DISCOUNT'
+}
+
+interface discountConditions {
+  SEQUENCE: 'SEQUENCE'
+  PERIOD: 'PERIOD'
+}
+
+class Movie2 {
+  private title: string
+  private runningTime: number
+  private fee: number
+  private discountConditions: discountConditions
+
+  private movieType: movieType
+  private discountAmount: number
+  private discountPercent: number
+
+  constructor(
+    title: string,
+    runningTime: number,
+    fee: number,
+    discountConditions: discountConditions,
+    movieType: movieType,
+    discountAmount: number,
+    discountPercent: number,
+  ) {
+    this.title = title
+    this.runningTime = runningTime
+    this.fee = fee
+    this.discountConditions = discountConditions
+    this.movieType = movieType
+    this.discountAmount = discountAmount
+    this.discountPercent = discountPercent
+  }
+
+  getMovieType() {
+    return this.movieType
+  }
+
+  setMovieType(movieType: movieType) {
+    this.movieType = movieType
+  }
+
+  getFee() {
+    return this.fee
+  }
+
+  setFee(fee: number) {
+    this.fee = fee
+  }
+
+  getDiscountConditions() {
+    return Collections.unmodifiableList(this.discountConditions)
+  }
+
+  setDiscountConditions(discountConditions: discountConditions) {
+    this.discountConditions = discountConditions
+  }
+
+  getDiscountAmount() {
+    return this.discountAmount
+  }
+
+  setDiscountAmount(discountAmount: number) {
+    this.discountAmount = discountAmount
+  }
+
+  getDiscountPercent() {
+    return this.discountPercent
+  }
+
+  setDiscountPercent(discountPercent: number) {
+    this.discountPercent = discountPercent
+  }
+}
