bd9fb09718d125549b7334ad36c4cef7357e2178
Youn Se Eun <tpdms717@gmail.com>
2023-04-08 08:47:56 +0900

    Create Customer class

---
+++ 상태분할/Customer.js
@@ -0,0 +1,7 @@
+"use strict";
+class Customer2 {
+    constructor(name, id) {
+        this.name = name;
+        this.id = id;
+    }
+}
---
+++ 상태분할/Customer.ts
@@ -0,0 +1,9 @@
+class Customer2 {
+  private name: string
+  private id: string
+
+  constructor(name: string, id: string) {
+    this.name = name
+    this.id = id
+  }
+}
